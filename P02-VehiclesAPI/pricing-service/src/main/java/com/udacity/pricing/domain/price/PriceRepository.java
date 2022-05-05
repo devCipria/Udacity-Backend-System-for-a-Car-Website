7433d95d100614a2780e5c33bcfb682668f7ea90
David Cipriati <devcipriati@gmail.com>
2022-05-05 10:20:28 -0400

    Add Eureka Server and convert Pricing-Service into a microservice

--- P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
@@ -1,8 +1,7 @@
 package com.udacity.pricing.domain.price;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-public class PriceRepository {
+public interface PriceRepository extends JpaRepository<Price, Long> {
 
 }
---
+++ P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO price (currency, price, vehicle_id) VALUES ('USD', 25000, 1);
+INSERT INTO price (currency, price, vehicle_id) VALUES ('USD', 17900, 2);
+INSERT INTO price (currency, price, vehicle_id) VALUES ('USD', 32500, 3);
+INSERT INTO price (currency, price, vehicle_id) VALUES ('USD', 14750, 4);
+INSERT INTO price (currency, price, vehicle_id) VALUES ('USD', 9500, 5);
+INSERT INTO price (currency, price, vehicle_id) VALUES ('USD', 43600, 6);
+INSERT INTO price (currency, price, vehicle_id) VALUES ('USD', 12875, 7);
+INSERT INTO price (currency, price, vehicle_id) VALUES ('USD', 52100, 8);
+INSERT INTO price (currency, price, vehicle_id) VALUES ('USD', 27985, 9);
+INSERT INTO price (currency, price, vehicle_id) VALUES ('USD', 39450, 10);
